21280fd619fb738bfea66735b4d446fc6a22eee3
ankush <ankush.wairagade@kapturecrm.cx>
2023-10-27 17:07:58 +0530

    springboot project added

---
+++ Kage-breaker/src/main/java/com/ankush/Kagebreaker/KageBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.ankush.Kagebreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KageBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KageBreakerApplication.class, args);
+	}
+
+}
---
+++ Kage-breaker/src/test/java/com/ankush/Kagebreaker/KageBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankush.Kagebreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KageBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
